3a721430feb2e8aec63fc2388107d352e7451b28
Mustafa <m.mustafaahmed@hotmail.com>
2018-11-26 04:48:50 +1100

    Directory structure changed, CBC mode implemeted

--- Symmetric/symmetric.py
+++ Symmetric/symmetric.py
@@ -4,13 +4,14 @@ import sys
 import argparse
 from wand.image import Image
 
-parser = argparse.ArgumentParser(prog="python3 ECB.py", description="Encrypt or decrypt an image file (in .jpg) using ECB mode of AES")
+parser = argparse.ArgumentParser(prog="python3 ECB.py", description="Encrypt or decrypt an image file (in .jpg) using AES mode.  Available options are:\nECB\nCBC")
 
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument('-e', '--encrypt', dest="enc_file", nargs=1, type=str, help="To encrypt the following image file", metavar="file_to_encrypt")
 group.add_argument('-d', '--decrypt', dest="dec_file", type=str, nargs=1, help="To decrypt the following image file", metavar="file_to_decrypt")
 parser.add_argument('-o', '--output', dest="out", nargs=1, type=str, help="Output file of decryption/encryption", required=True, metavar="output_file")
 parser.add_argument('-c', '--config', dest="config_file", nargs=1, type=str, help="Config file containing key", required=True, metavar="config_file")
+parser.add_argument('-m', '--mode', dest="mode", nargs=1, type=str, help="Modes: ECB| CBC", required=True, metavar="mode")
 args = parser.parse_args()
 
 pad = lambda s: s + bytes((AES.block_size - len(s) % AES.block_size) * \
@@ -22,13 +23,13 @@ def get_key(config_file):
     config.read(config_file)
     return config['KEY']['key']
 
-def encrypt(message, key):
-    cipher = AES.new(key, AES.MODE_ECB)
+def encrypt(message, key, mode):
+    cipher = AES.new(key, mode, "This is an IV123")
     msg = cipher.encrypt(pad(message))
     return bytes(msg)
 
-def decrypt(message, key):
-    cipher = AES.new(key, AES.MODE_ECB)
+def decrypt(message, key, mode):
+    cipher = AES.new(key, mode, "This is an IV123")
     dec = unpad(cipher.decrypt(message))
     return bytes(dec)
 
@@ -59,12 +60,14 @@ def main():
     if len(sys.argv) <= 1:
         parser.print_help()
 
+    modes = {'ECB': AES.MODE_ECB, 'CBC': AES.MODE_CBC}
+
     if args.enc_file:
         header, tail = get_bits_from_picture(args.enc_file[0])
         print("Getting the key....")
         key = get_key(args.config_file)
         print("Encrypting....")
-        enc = encrypt(tail, key)
+        enc = encrypt(tail, key, modes[args.mode[0]])
         store_to_file(args.out[0], header, enc)
         print("Done!")
 
@@ -73,7 +76,7 @@ def main():
         print("Getting the key....")
         key = get_key(args.config_file)
         print("Decrypting....")
-        dec = decrypt(tail, key)
+        dec = decrypt(tail, key, modes[args.mode[0]])
         store_to_file(args.out[0], header, dec)
         print("Done!")
 
